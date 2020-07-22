2254b80722269dc3f44132579e2e9c99d06fc925
Delia <you@example.com>
2020-07-22 13:32:29 -0400

    Fixed div selection to select only on popup divs

--- popup.js
+++ popup.js
@@ -8,16 +8,12 @@ function click(e) {
   });
   chrome.tabs.executeScript(null,
       {file:"script.js"});
-      // {code:"document.getElementById('coverLetter').value='" + e.target.id + "'"});
-      // {code:"document.getElementById('coverLetter').value='" + e.target.id + "'"});
-      // {code:"document.getElementById('coverLetter').value='" + e.target.id + "'"});
-      // {code:"document.getElementById('coverLetter').style.backgroundColor='" + e.target.id + "'"});
 
   window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
+  var divs = document.querySelectorAll('div.popup');
   for (var i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', click);
   }
